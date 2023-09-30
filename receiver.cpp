0a55aa82708d8b31fe924f75bb34d3614a29adca
Vinojitha <145708343+Vinojitha@users.noreply.github.com>
2023-09-30 15:04:20 -0300

    Create receiver.cpp
    receiver template code

---
+++ receiver.cpp
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Test By Vino");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+   
+    Serial.print("Received packet '");
+
+    
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
